b3422f7b1f7ebf58861d174fdfab38e1a70cecee
Niels Liisberg <nli@system-method.com>
2026-04-21 10:13:03 +0200

    info "Update AI key placeholder in config"

--- 02-config.sql
+++ 02-config.sql
@@ -1,7 +1,8 @@
 -- To acces open api you need to set the endpoint, model and key. 
 -- You can get a free key from openai.com and use any of the models listed on their website.
+-- https://platform.openai.com/api-keys
 create or replace variable sqlr2r.ai_endpoint   varchar(256)  default 'https://api.openai.com/v1/responses';
-create or replace variable sqlr2r.ai_key        varchar(2560) default 'xsk-proj-yy7HA6JDV-ydgdXrNVHH8CtJlXkGDTDNjTLPmxSmabE6nDlfo9gp9ok2JCRt0MLAQ4mYv67uAAT3BlbkFJGcXdmUEFwMW93-Ucy8tyyBY14NFj2e-kCbJF2w7OcUu8HvjbzhSH8E9uzbd0IW4l-z3qsMWJoA'; 
+create or replace variable sqlr2r.ai_key        varchar(2560) default '...Your open AI key goes here...'; 
 create or replace variable sqlr2r.ai_model      varchar(256)  default 'gpt-5-mini';
 
 -- Now what do we have:
